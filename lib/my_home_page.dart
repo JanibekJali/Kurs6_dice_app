826fe2fe75776fd469f0acb3588d3d9dbea4531a
JanibekJali <janibekjali@gmail.com>
2022-04-13 11:41:12 +0600

    Dice App Started

---
+++ lib/my_home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key, @required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffFF5353),
+      appBar: AppBar(
+        backgroundColor: const Color(0xffFF5353),
+        title: const Text(
+          'Тапшырма-05',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[],
+        ),
+      ),
+    );
+  }
+}
